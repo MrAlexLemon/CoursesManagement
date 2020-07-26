b0a30ec8db9c37ca6235247a2b30a9dbe982b2ba
Alex <lileynuk@gmail.com>
2020-07-26 15:44:30 +0300

    Added logging

---
+++ CoursesManagement.Common/CoursesManagement.Common/CoursesManagement.Common/AppOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoursesManagement.Common
+{
+    public class AppOptions
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ CoursesManagement.Common/CoursesManagement.Common/CoursesManagement.Common/Logging/SerilogOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoursesManagement.Common.Logging
+{
+    public class SerilogOptions
+    {
+        public bool ConsoleEnabled { get; set; }
+        public string Level { get; set; }
+    }
+}
